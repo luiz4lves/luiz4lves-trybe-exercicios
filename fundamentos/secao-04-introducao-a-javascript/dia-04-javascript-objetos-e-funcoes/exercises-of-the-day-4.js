304896196a5e5aadd04a84eacd855d067feeebe1
luiz4lves <luiz.ads@hotmail.com>
2022-09-30 22:16:34 -0300

    Finaliza exercícios de fixação

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercises-of-the-day-4.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercises-of-the-day-4.js
@@ -152,7 +152,7 @@
     //resultado: hamburguer, bife, acarajé;
 
     // >>>>>>>>> Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
-
+/*
     let names = {
         person1: 'João',
         person2: 'Maria',
@@ -162,10 +162,7 @@
       for (let index in names) {
         console.log('Olá ' + names[index] + '!');
       };
-      for (let index of names){
-        console.log(names[index]);
-      }
-
+*/
 /*
     //Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
 
@@ -179,3 +176,196 @@
         console.log(car[index]);
     }
 */
+
+        //FUNÇÕES 
+ /*   
+    let status = 'desligado';
+    let aceleracao = 0;
+    let rotacaoDoVolante = 0;
+
+    function ligarDesligar() {
+        if (status === 'desligado'){
+            status = 'ligado';
+        }
+        else {
+            status = 'desligado';
+        }
+        return status;
+    }
+
+    function acelera(a) {
+        aceleracao = aceleracao + a;
+        console.log('Acelerando à ' + aceleracao + 'm/s²'); 
+    }
+
+    function frear(f) {
+        aceleracao = aceleracao - f;
+        console.log('Desacelerando para ' + aceleracao + 'm/s²'); 
+    }
+
+    function volante(v) {
+        rotacaoDoVolante = v;
+        console.log('Volante girando ' + v + '°'); 
+    }
+
+    console.log(ligarDesligar()); 
+    
+    acelera(67);
+
+    frear(12);
+
+    volante(37.5);
+*/
+ /*
+    let status = 'deslogado';
+
+    function logarDeslogar() {
+    if (status === 'deslogado') {
+        status = 'logado';
+    } else {
+        status = 'deslogado';
+    }
+    }
+
+    console.log(status); // deslogado
+
+    logarDeslogar();
+    console.log(status); // logado
+
+    logarDeslogar();
+    console.log(status); // deslogado
+
+    console.log('O usuário está ' + status); // O usuário está deslogado
+*/
+/*
+     function soma(a, b) {
+     return a + b;
+    }
+
+    console.log(soma(1, 2)); // 7
+*/
+/*
+    function maiorNum(primeiroNum, segundoNum) {
+        if (primeiroNum > segundoNum) {
+        return primeiroNum + ' é maior que ' + segundoNum;
+        } else if (segundoNum > primeiroNum) {
+        return segundoNum + ' é maior que ' + primeiroNum;
+        } else {
+        return 'Os números são iguais';
+        }
+    }
+    
+    console.log(maiorNum(10, 20)); // 20 é maior que 10
+    console.log(maiorNum(2, -5)); // 2 é maior que -5
+    console.log(maiorNum(1, 1)); // Os números são iguais
+*/
+
+    //>>>>>>>>>>>>>>>>>> EXERCÍCIOS DO DIA
+    //Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para: Adição (a + b); Subtração (a - b); Multiplicação (a * b); Divisão (a / b); Módulo (a % b).
+/*
+    let a = 2;
+    let b = 4;
+
+    function sum(x, y){
+        let soma = x + y;
+        return soma;
+    }
+
+    function subtraction(x, y){
+        let subtacao = x - y;
+        return subtacao;
+    }
+
+    function multiplication(x, y){
+        let multiplicacao = x * y;
+        return multiplicacao;
+    }
+
+    function division(x, y){
+        let divisao = x / y;
+        return divisao;
+    }
+
+    function mod(x, y){
+        let modulo = x % y;
+        return modulo;
+    }
+    
+    console.log('Soma: ' + sum(a, b));
+    console.log('Subtração ' + subtraction(a, b));
+    console.log('Multiplicação: ' + multiplication(a, b));
+    console.log('Divisão: ' + division(a, b));
+    console.log('Módulo: ' + mod(a, b));
+*/
+    //Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+/*
+    function maiorMenor(a, b){
+        let small = [a];
+        if (a < b){
+            return '"a" é menor que "b"'
+        }
+        else if (a > b){
+            return '"a" é maior que "b"'
+        }
+        else {
+            return '"a" e "b" são iguais'
+        }
+    }
+    console.log(maiorMenor(6, 5));
+*/
+
+    //Faça um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+
+/*
+    function bigger (a, b, c){
+        let array = [a, b, c];
+        let i = [array[0]];
+        for (let index = 0; index < array.length; index += 1){
+            if (i[0] < array[index]){
+                i[0] = array[index];
+            }
+        }
+        return i[0];
+    }
+
+    console.log(bigger (9, 2, -1));
+*/
+
+    //Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+/*
+
+    function positiveOrNegative (z){
+        if (z > 0){
+            return 'positive';
+        }
+        else if (z < 0){
+            return 'negative';
+        }
+        else {
+            return 'zero'
+        }
+    }
+
+    console.log(positiveOrNegative(0));
+
+ */
+
+    //Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.Um ângulo será considerado inválido se não tiver um valor positivo.
+
+    function triangle(x, y, z){
+        if (x > 0 && y > 0 && z > 0){
+                let sum = x + y + z;
+                if (sum === 180){
+                return 'true';
+            }
+            else {
+                return 'false';
+            }
+            }
+        else {
+        return 'Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.Um ângulo será considerado inválido se não tiver um valor positivo.';
+        }
+    }
+
+    console.log(triangle(90, 45, 45));
