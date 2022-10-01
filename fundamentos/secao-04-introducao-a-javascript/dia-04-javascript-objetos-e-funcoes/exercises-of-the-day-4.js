c335da21430cc9ce2648171a11c3d023221ad376
luiz4lves <luiz.ads@hotmail.com>
2022-09-30 22:44:27 -0300

    Salvando exercícios

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercises-of-the-day-4.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercises-of-the-day-4.js
@@ -260,7 +260,8 @@
     console.log(maiorNum(1, 1)); // Os números são iguais
 */
 
-    //>>>>>>>>>>>>>>>>>> EXERCÍCIOS DO DIA
+    //>>>>>>>>>>>>>>>>>> EXERCÍCIOS DE FIXAÇÃO
+    
     //Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para: Adição (a + b); Subtração (a - b); Multiplicação (a * b); Divisão (a / b); Módulo (a % b).
 /*
     let a = 2;
