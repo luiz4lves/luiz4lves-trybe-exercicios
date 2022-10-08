a660a031638bffbaf4e27b8f5ff1f720d5817b3c
luiz4lves <luiz.ads@hotmail.com>
2022-10-08 16:38:18 -0300

    Parte 3 e 4 - Troca cor dos feriados ao clicar no botão Feriado e Cria botão Sexta-Feira

--- fundamentos/secao-05-javaScript-DOM-eventos-e-web-storage/dia-03-javaScript-eventos/calendar.js
+++ fundamentos/secao-05-javaScript-DOM-eventos-e-web-storage/dia-03-javaScript-eventos/calendar.js
@@ -15,6 +15,8 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 
+//>>>>>>>>>>>>>>>>>>> PARTE I <<<<<<<<<<<<<<<<<<<
+
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 let days = document.getElementsByTagName('div')[0];
@@ -48,14 +50,49 @@ function dias(){
   }
 }
 dias();
-  
-function buttonHoliday(buttonName){
-  const containerButton = document.getElementsByClassName('buttons-container')[0];
-  const button = document.createElement('button');
-  button.id = 'btn-holiday';
-  button.innerText = buttonName;
 
-  containerButton.appendChild(button);
+//>>>>>>>>>>>>>>>>>>> PARTE II <<<<<<<<<<<<<<<<<<<
+function buttonHoliday(buttonName){
+  const containerButtons = document.getElementsByClassName('buttons-container')[0];
+  const buttonHoliday = document.createElement('button');
+  buttonHoliday.id = 'btn-holiday';
+  buttonHoliday.innerText = buttonName;
+
+  containerButtons.appendChild(buttonHoliday);
 }
 
 buttonHoliday('Feriado');
+
+//>>>>>>>>>>>>>>>>>>> PARTE III <<<<<<<<<<<<<<<<<<<
+function changeColorHolidays(){
+
+const divButtonHoliday = document.getElementsByTagName('button')[0];
+const allHolidays = document.getElementsByClassName('holiday');
+const backgroundColorInitial = 'rgb(238,238,238)';
+const backgroundColorChange = 'purple';
+  
+  divButtonHoliday.addEventListener('click', function() {
+    for(let index = 0; index < allHolidays.length; index += 1){
+      if(allHolidays[index].style.backgroundColor === backgroundColorChange){
+        allHolidays[index].style.backgroundColor = backgroundColorInitial;
+        console.log(allHolidays);
+      }
+      else {
+      allHolidays[index].style.backgroundColor = backgroundColorChange;
+      }
+    }
+  })
+}
+changeColorHolidays();
+
+//>>>>>>>>>>>>>>>>>>> PARTE IV <<<<<<<<<<<<<<<<<<<
+function friday(buttonName){
+  const containerButtons = document.getElementsByClassName('buttons-container')[0];
+  const buttonFriday = document.createElement('button');
+  
+  buttonFriday.id = 'btn-friday';
+  buttonFriday.innerHTML = buttonName;
+  containerButtons.appendChild(buttonFriday);
+  
+}
+friday('Friday')
