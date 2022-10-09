dd768a5a11f86299b7ac6945bbdb37bf5f1518c8
luiz4lves <luiz.ads@hotmail.com>
2022-10-08 21:31:09 -0300

    Parte 5 - Dá dinamismo ao botão friday

--- fundamentos/secao-05-javaScript-DOM-eventos-e-web-storage/dia-03-javaScript-eventos/calendar.js
+++ fundamentos/secao-05-javaScript-DOM-eventos-e-web-storage/dia-03-javaScript-eventos/calendar.js
@@ -14,9 +14,9 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+const containerButtons = document.getElementsByClassName('buttons-container')[0];
 
 //>>>>>>>>>>>>>>>>>>> PARTE I <<<<<<<<<<<<<<<<<<<
-
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 let days = document.getElementsByTagName('div')[0];
@@ -53,7 +53,7 @@ dias();
 
 //>>>>>>>>>>>>>>>>>>> PARTE II <<<<<<<<<<<<<<<<<<<
 function buttonHoliday(buttonName){
-  const containerButtons = document.getElementsByClassName('buttons-container')[0];
+  
   const buttonHoliday = document.createElement('button');
   buttonHoliday.id = 'btn-holiday';
   buttonHoliday.innerText = buttonName;
@@ -66,10 +66,10 @@ buttonHoliday('Feriado');
 //>>>>>>>>>>>>>>>>>>> PARTE III <<<<<<<<<<<<<<<<<<<
 function changeColorHolidays(){
 
-const divButtonHoliday = document.getElementsByTagName('button')[0];
-const allHolidays = document.getElementsByClassName('holiday');
-const backgroundColorInitial = 'rgb(238,238,238)';
-const backgroundColorChange = 'purple';
+  const divButtonHoliday = document.getElementsByTagName('button')[0];
+  const allHolidays = document.getElementsByClassName('holiday');
+  const backgroundColorInitial = 'rgb(238,238,238)';
+  const backgroundColorChange = 'purple';
   
   divButtonHoliday.addEventListener('click', function() {
     for(let index = 0; index < allHolidays.length; index += 1){
@@ -87,12 +87,36 @@ changeColorHolidays();
 
 //>>>>>>>>>>>>>>>>>>> PARTE IV <<<<<<<<<<<<<<<<<<<
 function friday(buttonName){
-  const containerButtons = document.getElementsByClassName('buttons-container')[0];
+
   const buttonFriday = document.createElement('button');
   
   buttonFriday.id = 'btn-friday';
   buttonFriday.innerHTML = buttonName;
   containerButtons.appendChild(buttonFriday);
-  
+
 }
 friday('Friday')
+
+//>>>>>>>>>>>>>>>>>>> PARTE V <<<<<<<<<<<<<<<<<<<
+function changeText(){
+
+  const allFridays = document.getElementsByClassName('friday');
+  const divButtonFriday = document.getElementById('btn-friday');
+  const textChange = 'Sextou';
+  const arrayInitial = [4, 11, 18, 25];
+  divButtonFriday.addEventListener('click', function(){ 
+    
+      for (let index = 0; index < allFridays.length; index += 1){   
+          
+        if(allFridays[index].innerHTML !== textChange){
+        allFridays[index].innerHTML = textChange;
+        }
+        else {
+        allFridays[index].innerHTML = arrayInitial[index];
+      }
+    }
+
+  })
+
+}
+changeText();
